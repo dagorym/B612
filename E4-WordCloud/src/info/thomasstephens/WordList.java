8c72f3c3f2e2d0f51806516c6bf1fd1380f83408
Tom Stephens <thomas.stephens@gmail.com>
2019-08-10 19:58:58 -0700

    Add stubs for WordList class

---
+++ E4-WordCloud/src/info/thomasstephens/WordList.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package info.thomasstephens;
+
+/**
+ * Object to store all the words and their values for the
+ * word cloud.  This class uses a map as the underlying data
+ * structure to hold the data with the word as the key and the
+ * count as the value.  It requires a DropList object to be
+ * passed in to ignore words.
+ * 
+ * @date Created: Aug 10, 2019
+ * @date Modified: Aug 10, 2019
+ * @author Tom Stephens
+ */
+public class WordList {
+
+}
---
+++ E4-WordCloud/test/info/thomasstephens/WordListTest.java
@@ -0,0 +1,14 @@
+package info.thomasstephens;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordListTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
